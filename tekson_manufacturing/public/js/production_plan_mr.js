1bae3e0f3f83b900f6a310738345fd3d4a48684c
OSDuo Tech <developer@osduotech.com>
2026-08-10 20:44:18 +0530

    fix: Add date picker — planner selects any planned date for MR generation

--- tekson_manufacturing/public/js/production_plan_mr.js
+++ tekson_manufacturing/public/js/production_plan_mr.js
@@ -5,25 +5,36 @@ frappe.ui.form.on('Production Plan', {
     refresh: function(frm) {
         if (frm.doc.docstatus === 1) {
             frm.add_custom_button(__('Generate Material Requests'), function() {
-                frappe.call({
-                    method: 'tekson_manufacturing.planning.material_planning_service.generate_daily_material_requests',
-                    args: { production_plan: frm.doc.name },
-                    freeze: true,
-                    freeze_message: __('Generating Material Requests...'),
-                    callback: function(r) {
-                        if (r.message && r.message.created_mrs) {
-                            let mrs = r.message.created_mrs;
-                            let msg = __('Created {0} Material Request(s):<br>', [mrs.length]);
-                            mrs.forEach(function(mr) {
-                                msg += '<br><a href="/app/material-request/' + mr + '">' + mr + '</a>';
-                            });
-                            frappe.msgprint({title: __('Material Requests Created'), message: msg, indicator: 'green'});
-                            frm.refresh();
-                        } else {
-                            frappe.msgprint({title: __('No MRs Created'), message: r.message.message || __('All materials already in WIP'), indicator: 'blue'});
-                        }
+                let d = new frappe.ui.Dialog({
+                    title: __('Generate Material Requests'),
+                    fields: [
+                        {fieldname: 'planned_date', fieldtype: 'Date', label: __('Planned Start Date'), 
+                         default: frappe.datetime.get_today(), reqd: 1},
+                    ],
+                    primary_action_label: __('Generate'),
+                    primary_action: function(values) {
+                        d.hide();
+                        frappe.call({
+                            method: 'tekson_manufacturing.planning.material_planning_service.generate_daily_material_requests',
+                            args: { production_plan: frm.doc.name, planned_date: values.planned_date },
+                            freeze: true,
+                            freeze_message: __('Generating Material Requests...'),
+                            callback: function(r) {
+                                let res = r.message || {};
+                                if (res.created_mrs && res.created_mrs.length > 0) {
+                                    let msg = __('<b>{0}</b> MR(s) for <b>{1}</b>:<br>', [res.created_mrs.length, res.planned_date]);
+                                    (res.details || []).forEach(function(d) {
+                                        msg += '<br><a href="/app/material-request/' + d.name + '">' + d.name + '</a> — ' + d.department_wip + ' (' + d.items + ' items)';
+                                    });
+                                    frappe.msgprint({title: __('✅ Generated'), message: msg, indicator: 'green'});
+                                } else {
+                                    frappe.msgprint({title: __('No MRs'), message: res.message || __('All materials already in WIP for {0}').format(values.planned_date), indicator: 'blue'});
+                                }
+                            }
+                        });
                     }
                 });
+                d.show();
             });
         }
     }
