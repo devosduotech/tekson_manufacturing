abca81e58a80b79b31b12ba837fd918697830db0
OSDuo Tech <developer@osduotech.com>
2026-08-11 00:03:27 +0530

    fix: Restore overrides/__init__.py, clean duplicates

--- tekson_manufacturing/tekson_manufacturing/report/material_transfer_pick_list/material_transfer_pick_list.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, OSDuo Tech LLP and contributors
-// For license information, please see license.txt
-
-frappe.query_reports["Material Transfer Pick List"] = {
-	"filters": [
-
-	]
-};
