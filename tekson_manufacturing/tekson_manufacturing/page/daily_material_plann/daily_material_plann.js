70fbbca884e3415fe5d58706f082ee256e970d3c
Developer - OSDuo Tech <developer@osduotech.com>
2026-08-10 18:13:51 +0000

    fix: Remove auto-generated page folders

--- tekson_manufacturing/tekson_manufacturing/page/daily_material_plann/daily_material_plann.js
+++
@@ -1,7 +0,0 @@
-frappe.pages['daily-material-plann'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Daily Material Planning',
-		single_column: true
-	});
-}
