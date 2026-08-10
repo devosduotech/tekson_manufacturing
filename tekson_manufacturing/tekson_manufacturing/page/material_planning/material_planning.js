ace21a2e86d9d422bf6c9a4c928f9d0b6b1af222
OSDuo Tech <developer@osduotech.com>
2026-08-10 23:54:50 +0530

    fix: Use Frappe DatePicker widget for DD/MM/YYYY format

--- tekson_manufacturing/tekson_manufacturing/page/material_planning/material_planning.js
+++ tekson_manufacturing/tekson_manufacturing/page/material_planning/material_planning.js
@@ -1,18 +1,23 @@
 frappe.pages['material-planning'].on_page_load = function(wrapper) {
     let page = frappe.ui.make_app_page({parent: wrapper, title: 'Daily Material Planning', single_column: true});
     
-    let html = '<div style="padding:30px; max-width:800px;"><h3>Daily Material Planning</h3><p>Generate Material Requests for stores.</p><hr><div class="row"><div class="col-sm-6"><label>Production Plan</label><select id="pp_select" class="form-control"></select></div><div class="col-sm-3"><label>Planned Start Date</label><input type="date" id="plan_date" class="form-control"></div><div class="col-sm-3"><br><button id="gen_btn" class="btn btn-primary">Generate MRs</button></div></div><div id="result_area" style="margin-top:20px;"></div></div>';
+    let html = '<div style="padding:30px; max-width:800px;"><h3>Daily Material Planning</h3><p>Generate Material Requests for stores.</p><hr><div class="row"><div class="col-sm-6"><label>Production Plan</label><select id="pp_select" class="form-control"></select></div><div class="col-sm-3"><label>Planned Start Date</label><div id="plan_date_wrapper"></div></div><div class="col-sm-3"><br><button id="gen_btn" class="btn btn-primary">Generate MRs</button></div></div><div id="result_area" style="margin-top:20px;"></div></div>';
     page.body.html(html);
+    
+    var dateField = frappe.ui.form.make_control({
+        parent: page.body.find('#plan_date_wrapper'),
+        df: {fieldtype: 'Date', fieldname: 'plan_date', label: '', reqd: 1},
+        render_input: true,
+    });
+    dateField.set_value(frappe.datetime.get_today());
     
     frappe.call({method:'tekson_manufacturing.www.mes.daily_material_planning.search_production_plans',args:{txt:''},callback:function(r){
         var sel = page.body.find('#pp_select');
         (r.message||[]).forEach(function(pp){sel.append('<option value="'+pp.name+'">'+pp.name+'</option>');});
     }});
     
-    page.body.find('#plan_date').val(frappe.datetime.get_today());
-    
     page.body.find('#gen_btn').on('click',function(){
-        var pp = page.body.find('#pp_select').val(), dt = page.body.find('#plan_date').val();
+        var pp = page.body.find('#pp_select').val(), dt = dateField.get_value();
         if(!pp){frappe.msgprint('Select a Production Plan');return;}
         var btn = page.body.find('#gen_btn');btn.prop('disabled',true).text('Generating...');
         frappe.call({method:'tekson_manufacturing.planning.material_planning_service.generate_daily_material_requests',args:{production_plan:pp,planned_date:dt},callback:function(r){
