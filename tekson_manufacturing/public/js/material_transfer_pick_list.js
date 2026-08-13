9e3c7f3c0e5933fc6da22cb8fc5bda1b24605798
OSDuo Tech <developer@osduotech.com>
2026-08-13 21:27:41 +0530

    fix: Temporarily disable Pick List button (getdoctype() error)

--- tekson_manufacturing/public/js/material_transfer_pick_list.js
+++ tekson_manufacturing/public/js/material_transfer_pick_list.js
@@ -2,14 +2,16 @@
  * Material Transfer Pick List - Work Order List View Integration
  * 
  * Adds "Pick List" button to Work Order List View for quick access.
+ * TEMPORARILY DISABLED - getdoctype() error
  */
 
-var wos = frappe.listview_settings['Work Order'] || {};
-wos.onload = function(listview) {
-    listview.page.add_action_item(__('Pick List'), function() {
-        let wo = listview.get_checked_items().length === 1 ? listview.get_checked_items()[0].name : '';
-        let url = '/app/query-report/Material Transfer Pick List' + (wo ? '?work_order=' + encodeURIComponent(wo) : '');
-        window.open(url, '_blank');
-    });
-};
-frappe.listview_settings['Work Order'] = wos;
+// Disabled - causing getdoctype() error in ERPNext
+// var wos = frappe.listview_settings['Work Order'] || {};
+// wos.onload = function(listview) {
+//     listview.page.add_action_item(__('Pick List'), function() {
+//         let wo = listview.get_checked_items().length === 1 ? listview.get_checked_items()[0].name : '';
+//         let url = '/app/query-report/Material Transfer Pick List' + (wo ? '?work_order=' + encodeURIComponent(wo) : '');
+//         window.open(url, '_blank');
+//     });
+// };
+// frappe.listview_settings['Work Order'] = wos;
