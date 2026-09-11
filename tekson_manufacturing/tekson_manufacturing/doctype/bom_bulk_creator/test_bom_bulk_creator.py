6461a2f0a34d24ccd7224604dd470d729eb74a26
Developer - OSDuo Tech <developer@osduotech.com>
2026-09-11 12:43:06 +0000

    fix: BOM Bulk Creator fixes for form save and module resolution

---
+++ tekson_manufacturing/tekson_manufacturing/doctype/bom_bulk_creator/test_bom_bulk_creator.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, OSDuo Tech LLP and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBOMBulkCreator(FrappeTestCase):
+	pass
